7bb95e2ae3cfca7ca750f549e5489d60f63f7257
thaibangHOANG <thaibang1983hn@gmail.com>
2026-04-24 22:55:55 +0200

    rename and add register file

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = `${environment.apiUrl}/auth`;
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any) {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: any) {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  } 
+}
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
 export class RegisterComponent {
 
+  username = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  register() {
+    this.authService.register({
+      username: this.username,
+      password: this.password
+    }).subscribe({
+      next: () => alert('Register success'),
+      error: err => console.error(err)
+    });
+  }
 }
