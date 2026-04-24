85f7f179e62a120f7895460896f0214aa78fe22e
thaibangHOANG <thaibang1983hn@gmail.com>
2026-04-24 19:45:38 +0200

    create the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login')
+        .then(m => m.LoginComponent)
+  },
+
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register')
+        .then(m => m.RegisterComponent)
+  },
+
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./features/tasks/task-list/task-list')
+        .then(m => m.TaskListComponent)
+  },
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+
+];
