98b4a22f802b2203b094013c6767b80514b988b6
thaibangHOANG <thaibang1983hn@gmail.com>
2026-04-25 21:22:29 +0200

    add login

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class LoginComponent {
+
+  username = '';
+  password = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  login() {
+
+    const data = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.login(data).subscribe({
+      next: (res: any) => {
+
+        // save token
+        localStorage.setItem('token', res.token);
+
+        alert('Login success');
+
+        // redirect
+        this.router.navigate(['/tasks']);
+      },
+
+      error: (err) => {
+        console.error(err);
+        alert('Login failed');
+      }
+    });
+
+  }
 
 }
