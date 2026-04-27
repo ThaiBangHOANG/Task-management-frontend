3554d89dd0580175ab303b58709d66e6e2cae82c
thaibangHOANG <thaibang1983hn@gmail.com>
2026-04-27 11:30:36 +0200

    modify the structure

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  
+  private apiUrl = 'http://localhost:5150/api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  createTask(task: any): Observable<any> {
+    return this.http.post(this.apiUrl, task);
+  }
 }
--- src/app/features/tasks/task-list/task-list.ts
+++ src/app/features/tasks/task-list/task-list.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../../core/services/task.service';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
 export class TaskListComponent {
 
+  tasks: any[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe({
+      next: (data) => {
+        console.log('Tasks:', data);
+        this.tasks = data;
+      },
+      error: (err) => {
+        console.error('Error loading tasks:', err);
+      }
+    });
+  }
 }
