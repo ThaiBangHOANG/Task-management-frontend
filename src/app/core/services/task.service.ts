ebba7646deafbfa9b13689c72f5ff955f4448377
thaibangHOANG <thaibang1983hn@gmail.com>
2026-04-28 16:41:01 +0200

    Configure production and build

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -2,74 +2,64 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/task.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5150/api/tasks';
+  private apiUrl = `${environment.apiUrl}/tasks`;
 
   constructor(private http: HttpClient) {}
 
-getTasks(
-  page: number,
-  pageSize: number,
-  search?: string,
-  status?: number,
-  isCompleted?: boolean,
-  sortBy?: string,
-  sortDescending?: boolean
-) {
+  getTasks(
+    page: number,
+    pageSize: number,
+    search?: string,
+    status?: number,
+    isCompleted?: boolean,
+    sortBy?: string,
+    sortDescending?: boolean,
+  ) {
+    let params: any = {
+      page,
+      pageSize,
+    };
 
-  let params: any = {
-    page,
-    pageSize
-  };
+    if (search) params.search = search;
 
-  if (search)
-    params.search = search;
+    if (status !== undefined) params.status = status;
 
-  if (status !== undefined)
-    params.status = status;
+    if (isCompleted !== undefined) params.isCompleted = isCompleted;
 
-  if (isCompleted !== undefined)
-    params.isCompleted = isCompleted;
+    if (sortBy) params.sortBy = sortBy;
 
-  if (sortBy)
-    params.sortBy = sortBy;
+    if (sortDescending !== undefined) params.sortDescending = sortDescending;
 
-  if (sortDescending !== undefined)
-    params.sortDescending = sortDescending;
-
-  return this.http.get(this.apiUrl, {
-    params
-  });
-
-}
+    return this.http.get(this.apiUrl, {
+      params,
+    });
+  }
 
   createTask(task: any): Observable<any> {
     return this.http.post(this.apiUrl, task);
   }
 
   deleteTask(id: number) {
-  return this.http.delete(
-    `http://localhost:5150/api/tasks/${id}`
-  );
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
   getTaskById(id: number) {
-  return this.http.get(
-    `http://localhost:5150/api/tasks/${id}`
-  );
-}
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
 
-updateTask(id: number,task: Task) {
-  const request = {
-    title: task.title,
-    description: task.description,
-    status: task.status
-  };
+  updateTask(id: number, task: Task) {
+    const request = {
+      title: task.title,
+      description: task.description,
+      status: task.status,
+    };
 
-  return this.http.put(`${this.apiUrl}/${task.id}`, request);
-}
+    return this.http.put(`${this.apiUrl}/${task.id}`, request);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://localhost:5150/api'
 };
