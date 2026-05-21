30078a8bae97baaece878be978dbf4e04e18aae5
thaibangHOANG <thaibang1983hn@gmail.com>
2026-05-22 00:07:28 +0200

    add dockerignore and nginx.conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://task-api-container.livelywave-91602587.francecentral.azurecontainerapps.io'
+  apiUrl: 'https://task-api-container.livelywave-91602587.francecentral.azurecontainerapps.io/api'
 };
